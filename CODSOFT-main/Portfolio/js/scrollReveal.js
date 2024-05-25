20fe46657c9e047f80f44c15f7761105181f2821
Chandrika Manchala <127883024+chandrika00@users.noreply.github.com>
2024-05-25 22:18:22 +0530

    Delete CODSOFT-main/Portfolio directory

--- CODSOFT-main/Portfolio/js/scrollReveal.js
+++
@@ -1,5 +0,0 @@
-window.sr = ScrollReveal();
-sr.reveal('.edu-grid', {duration:1000});
-sr.reveal('#skills-icons > div', {duration:1000},50);
-sr.reveal('.jm-item-wrapper',{duration:1000},100);
-//sr.reveal('#projects', {duration:1000});
